be10d015e6942576c93b441bc49de6f3b805ee0a
Pumbaba <t.fanac@proton.me>
2023-11-29 23:16:19 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Windows Remove Bloatware
+
+This is a simple script which removes a blacklist of bloatware from windows 10 or 11. You can add or remove apps from the list as you need it.
+
+## How to use
+
+1. Open powershell as Administrator
+2. Run the following commands
+
+```powershell
+git clone git@github.com:pumbaba/windows11-remove-bloatware.git
+cd windows11-remove-bloatware
+powershell -ExecutionPolicy Bypass -File remove-bloatware.ps1
+```
+
+## Customize Blacklist
+
+1. Run `Get-AppxPackage –AllUsers | Select Name` to get a list of all installed apps
+2. Open the `remove-bloatware.ps1` and add apps as needed to the blacklist.
---
+++ remove-bloatware.ps1
@@ -0,0 +1,69 @@
+
+$blacklist = @(
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.People",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "MicrosoftCorporationII.QuickAssist",
+    "Microsoft.WindowsMaps",
+    "Microsoft.WindowsAlarms",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "WindowsFeedbackHub",
+    "Microsoft.MicrosoftOfficeHub",
+    "Clipchamp.Clipchamp",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.OutlookForWindows",
+    "Microsoft.YourPhone",
+    "MicrosoftTeams",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.Todos",
+    "Microsoft.GamingApp",
+    "microsoft.windowscommunicationsapps"
+)
+
+Set-Location -Path $PSScriptRoot
+
+# Check if the current user has administrative privileges
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+if (-not $isAdmin) {
+    Write-Host "Please run this script as an administrator."
+    exit
+}
+
+# Removes a Windows 10 bloatware app
+function Remove-Bloatware {
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]$Name
+    )
+    
+    try {
+        $package = Get-AppxPackage -AllUsers -Name "*$Name*" -ErrorAction Stop
+        if ($package) {
+            $package | Remove-AppxPackage -AllUsers -ErrorAction Stop
+            Write-Host "Successfully uninstalled: $Name"
+        }
+        else {
+            Write-Host "Package '$Name' not found."
+        }
+    }
+    catch {
+        Write-Host "Failed to uninstall '$Name'. Error: $($_.Exception.Message)"
+    }
+}
+
+foreach ($app in $blacklist) {
+    Remove-Bloatware -Name $app
+}
+
+Write-Host "Press Enter to exit..."
+$null = Read-Host
+
+
+
